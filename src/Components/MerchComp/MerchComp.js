6e4411a4b9d45aa1f8f515b902cebca8d79f4e40
Paul G <35400575+notpaulgaddy@users.noreply.github.com>
2022-12-19 19:59:54 -0500

    Add files via upload

--- src/Components/MerchComp/MerchComp.js
+++ src/Components/MerchComp/MerchComp.js
@@ -1,8 +1,7 @@
 import React from "react";
-import styles from "./MerchComp.css";
+import "./MerchComp.css";
 import { Card } from "react-bootstrap";
-import { render } from "@testing-library/react";
-
+import Button from 'react-bootstrap/Button';
 const Cards = () => {
   const cardInfo = [
     {
@@ -11,86 +10,159 @@ const Cards = () => {
         title: "AHTD Tour Poster Tee",
         text: "$50.00",
         category: "shirt",
-        status:"SOLD OUT"
+        status:"SOLD OUT",
+        exclusivity: "None"
       },
       {
         image:
           "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---TOUR-TEE-FRONT_800x.png?v=1663686212",
         title: "AHTD Tour Tee",
         text: "$50.00",
-        category: "shirt"
+        category: "shirt",
+        status:"Available",
+        exclusivity: "None"
       },
       {
         image:
           "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---XOHF-TEE-BACK_880x.png?v=1665085111",
         title: "AHTD Tour XOHF Tee",
         text: "$50.00",
-        category: "shirt"
+        category: "shirt",
+        status:"Available",
+        exclusivity: "None"
       },
       {
         image:
           "https://cdn.shopify.com/s/files/1/1429/6260/products/XO-CHROME-LOGO-TEE-BLACK_880x.png?v=1651001065",
         title: "DAWN FM Chrome XO Logo Tee",
         text: "$40.00",
-        category: "shirt"
+        category: "shirt",
+        status:"Available",
+        exclusivity: "None"
       },{
         image:
           "https://cdn.shopify.com/s/files/1/1429/6260/products/XO-CHROME-LOGO-TEE-WHITE_880x.png?v=1651001070",
         title: "DAWN FM Logo Tee",
         text: "$50.00",
-        category: "shirt"
+        category: "shirt",
+        status:"Available",
+        exclusivity: "None"
       },
       {
         image:
           "https://cdn.shopify.com/s/files/1/1429/6260/products/blackxoclassic_880x.png?v=1663951164",
         title: "XO Classic Logo Tee",
         text: "$40.00",
-        category: "shirt"
+        category: "shirt",
+        status:"Available",
+        exclusivity: "None"
       },
       {
         image:
           "https://cdn.shopify.com/s/files/1/1429/6260/products/whitexologo_880x.png?v=1663951191",
         title: "XO Classic Logo Tee",
         text: "$40.00",
-        category: "shirt"
+        category: "shirt",
+        status:"Available",
+        exclusivity: "None"
       },
       {
         image:
           "https://cdn.shopify.com/s/files/1/1429/6260/products/DAWNFMPLANETEE_880x.png?v=1642780477",
         title: "DAWN FM Plane Tee",
         text: "$38.00",
-        category: "shirt"
+        category: "shirt",
+        status:"Available",
+        exclusivity: "None"
       },{
         image:
           "https://cdn.shopify.com/s/files/1/1429/6260/products/DFM-COVER-TEE-BLACK-FRONT_880x.png?v=1641073291",
         title: "DAWN FM Cover Tee",
         text: "$40.00",
-        category: "shirt"
+        category: "shirt",
+        status:"Available",
+        exclusivity: "None"
       },
       {
         image:
           "https://cdn.shopify.com/s/files/1/1429/6260/products/DFM-TEE-BLACK-FRONT_880x.png?v=1641073237",
         title: "DAWN FM Tee",
         text: "$38.00",
-        category: "shirt"
+        category: "shirt",
+        status:"Available",
+        exclusivity: "None"
       },{
         image:
           "https://cdn.shopify.com/s/files/1/1429/6260/products/DFM-CHROME-TITLE-TEE-BLACK-FRONT_880x.png?v=1641073307",
         title: "DAWN FM Chrome Title Tee",
         text: "$38.00",
-        category: "shirt"
+        category: "shirt",
+        status:"Available",
+        exclusivity: "None"
       },
       {
         image:
           "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-LS-FRONT_800x.png?v=1663686473",
         title: "AHTD Tour Poster Longsleeve Tee",
         text: "$60.00",
-        category: "shirt"
+        category: "shirt",
+        status:"Available",
+        exclusivity: "None"
+      },{
+        image:
+          "https://cdn.shopify.com/s/files/1/1429/6260/products/blacktruckerfront_800x.png?v=1663685658",
+        title: "AHTD TOUR FOAM TRUCKER Black",
+        text: "$35.00",
+        category: "hat",
+        status:"Available",
+        exclusivity: "Limited Edition"
+      },{
+        image:
+          "https://cdn.shopify.com/s/files/1/1429/6260/products/whitetruckerfront_800x.png?v=1663685469",
+        title: "AHTD TOUR FOAM TRUCKER White",
+        text: "$35.00",
+        category: "hat",
+        status:"Available",
+        exclusivity: "Limited Edition"
+      },{
+        image:
+          "https://cdn.shopify.com/s/files/1/1429/6260/products/Dawn-fm_02_a6c42d96-940d-438f-8713-c692d16a792a_880x.png?v=1663947402",
+        title: "AHTD TOUR FM Keychain",
+        text: "$30.00",
+        category: "keychain",
+        status:"Available",
+        exclusivity: "Limited Edition"
+      },{
+        image:
+          "https://cdn.shopify.com/s/files/1/1429/6260/products/XO_02_880x.png?v=1663947333",
+        title: "AHTD TOUR KO Keychain",
+        text: "$30.00",
+        category: "keychain",
+        status:"Available",
+        exclusivity: "Limited Edition"
+      },{
+        image:
+          "https://cdn.shopify.com/s/files/1/1429/6260/products/1rect_880x.png?v=1669154014",
+        title: "0:00:01",
+        text: "$145.00",
+        category: "painting",
+        status:"Available",
+        exclusivity: "Limited Edition"
       }
   ];
   const renderCard = (card, index) => {
+    let exclusive;
+    let status;
+    if(card.exclusivity == "Limited Edition"){
+      exclusive = <div><Button style={{margin: "auto"}} variant="warning">Limited Edition</Button></div>
+    }
+    if(card.status=="SOLD OUT"){
+      status = <div><Button style={{margin: "auto"}} variant="primary">SOLD OUT</Button></div>
+    }else{
+      status = <div><Button style={{margin: "auto"}} >Available</Button></div>
+    }
     return (
-      <Card style={{ width: "18rem" }} key={index} className="box">
+      <Card style={{ width: "18rem",marginBottom:"10px"}} key={index} className="square border border-0">
         <Card.Img
           variant="top"
           src={card.image}
@@ -98,6 +170,8 @@ const Cards = () => {
         <Card.Body>
           <Card.Title style={{textAlign: "center"}}>{card.title}</Card.Title>
           <Card.Text style={{textAlign: "center"}}>{card.text}</Card.Text>
+          <Card.Text className="limitedButton col text-center">{exclusive}</Card.Text>
+          <Card.Text className="limitedButton col text-center">{status}</Card.Text>
         </Card.Body>
       </Card>
     );
---
+++ src/Components/Videos/Videos.js
@@ -0,0 +1,136 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarLink from "../NavbarLink/NavbarLink";
+import "./Videos.css";
+class Videos extends React.Component{
+    render(){
+        return(
+            <div>
+            <NavbarLink/>
+            <main class="container" className="fullBox">
+                <div class="row"> {/*Each row*/}
+                    <div class="col-md-8 leftSide">{/*Left side*/}
+                        <div> {/*Top left, biggest*/}
+                            <iframe class="biggest grid-item" src="https://www.youtube.com/embed/2fDzCWNS3ig" title="The Weeknd - Out of Time (Official Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <div class="col-md-6 grid-item" className="bottomLeft">{/*2 bottom left videos*/}
+                            <iframe className="smallerLR" src="https://www.youtube.com/embed/rhTl_OyehF8">
+                            </iframe>
+                            <iframe className="smallerLR grid-item" style={{display:"inline"}}
+                            src="https://www.youtube.com/embed/UKunvvN2iCk">
+                            </iframe>
+                        </div>
+                    </div>
+                    <div class="col-md-4 grid-item rightSide">
+                        <div class="row">
+                        <iframe className="rightTB" src="https://www.youtube.com/embed/iOa7uE23qc4">
+                        </iframe>
+                        </div>
+                        <div class="row">
+                        <iframe className="rightTB" src="https://www.youtube.com/embed/VafTMsrnSTU">
+                        </iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row"> {/*Each row*/}
+                    <div class="col-md-4 grid-item rightSide">
+                        <div class="row">
+                        <iframe className="rightTB" src="https://www.youtube.com/embed/iOa7uE23qc4">
+                        </iframe>
+                        </div>
+                        <div class="row">
+                        <iframe className="rightTB" src="https://www.youtube.com/embed/VafTMsrnSTU">
+                        </iframe>
+                        </div>
+                    </div>
+                    <div class="col-md-8 leftSide">{/*Left side*/}
+                        <div> {/*Top left, biggest*/}
+                            <iframe class="biggest grid-item" src="https://www.youtube.com/embed/2fDzCWNS3ig" title="The Weeknd - Out of Time (Official Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <div class="col-md-6 grid-item" className="bottomLeft">{/*2 bottom left videos*/}
+                            <iframe className="smallerLR" src="https://www.youtube.com/embed/rhTl_OyehF8">
+                            </iframe>
+                            <iframe className="smallerLR grid-item" style={{display:"inline"}}
+                            src="https://www.youtube.com/embed/UKunvvN2iCk">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row"> {/*Each row*/}
+                    <div class="col-md-4 grid-item rightSide">
+                        <div class="row">
+                        <iframe className="rightTB" src="https://www.youtube.com/embed/iOa7uE23qc4">
+                        </iframe>
+                        </div>
+                        <div class="row">
+                        <iframe className="rightTB" src="https://www.youtube.com/embed/VafTMsrnSTU">
+                        </iframe>
+                        </div>
+                    </div>
+                    <div class="col-md-8 leftSide">{/*Left side*/}
+                        <div> {/*Top left, biggest*/}
+                            <iframe class="biggest grid-item" src="https://www.youtube.com/embed/2fDzCWNS3ig" title="The Weeknd - Out of Time (Official Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <div class="col-md-6 grid-item" className="bottomLeft">{/*2 bottom left videos*/}
+                            <iframe className="smallerLR" src="https://www.youtube.com/embed/rhTl_OyehF8">
+                            </iframe>
+                            <iframe className="smallerLR grid-item" style={{display:"inline"}}
+                            src="https://www.youtube.com/embed/UKunvvN2iCk">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row"> {/*Each row*/}
+                    <div class="col-md-4 grid-item rightSide">
+                        <div class="row">
+                        <iframe className="rightTB" src="https://www.youtube.com/embed/iOa7uE23qc4">
+                        </iframe>
+                        </div>
+                        <div class="row">
+                        <iframe className="rightTB" src="https://www.youtube.com/embed/VafTMsrnSTU">
+                        </iframe>
+                        </div>
+                    </div>
+                    <div class="col-md-8 leftSide">{/*Left side*/}
+                        <div> {/*Top left, biggest*/}
+                            <iframe class="biggest grid-item" src="https://www.youtube.com/embed/2fDzCWNS3ig" title="The Weeknd - Out of Time (Official Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <div class="col-md-6 grid-item" className="bottomLeft">{/*2 bottom left videos*/}
+                            <iframe className="smallerLR" src="https://www.youtube.com/embed/rhTl_OyehF8">
+                            </iframe>
+                            <iframe className="smallerLR grid-item" style={{display:"inline"}}
+                            src="https://www.youtube.com/embed/UKunvvN2iCk">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row"> {/*Each row*/}
+                    <div class="col-md-4 grid-item rightSide">
+                        <div class="row">
+                        <iframe className="rightTB" src="https://www.youtube.com/embed/iOa7uE23qc4">
+                        </iframe>
+                        </div>
+                        <div class="row">
+                        <iframe className="rightTB" src="https://www.youtube.com/embed/VafTMsrnSTU">
+                        </iframe>
+                        </div>
+                    </div>
+                    <div class="col-md-8 leftSide">{/*Left side*/}
+                        <div> {/*Top left, biggest*/}
+                            <iframe class="biggest grid-item" src="https://www.youtube.com/embed/2fDzCWNS3ig" title="The Weeknd - Out of Time (Official Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <div class="col-md-6 grid-item" className="bottomLeft">{/*2 bottom left videos*/}
+                            <iframe className="smallerLR" src="https://www.youtube.com/embed/rhTl_OyehF8">
+                            </iframe>
+                            <iframe className="smallerLR grid-item" style={{display:"inline"}}
+                            src="https://www.youtube.com/embed/UKunvvN2iCk">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+                </main>
+            </div>
+        )
+    }
+}
+export default Videos;
