20fa9fdc4ceddc623e3591e5f8a93e7f52b85b9b
Paul G <35400575+notpaulgaddy@users.noreply.github.com>
2022-12-24 01:40:42 -0500

    Add files via upload
    Signup basics working, Routes for merch now work with the product id, dynamic item data showing up, moved item data to clothingData.js

---
+++ App/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Home from "../Home/Home";
+import Merch from "../Merch/Merch";
+import Videos from "../Videos/Videos";
+import Signup from "../Signup/Signup";
+import Login from "../Login/Login";
+import ProductPage from '../ProductPage/ProductPage';
+
+import {BrowserRouter,Route,Routes,Switch} from "react-router-dom";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/Videos" element={<Videos/>}/>
+          <Route exact path="/Login" element={<Login/>}/>
+          <Route exact path="/Signup" element={<Signup/>}/>
+          <Route exact path="/Merch" element={<Merch/>}/>
+          <Route exact path="/Merch/:productId" element={<ProductPage/>}/>
+          <Route path="*" element={<h1>Not found</h1>}/>
+        </Routes>
+      </BrowserRouter>
+
+  );
+}
+
+export default App;
