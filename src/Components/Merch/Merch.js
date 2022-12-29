bc8f70e4061ea9b6cc5292faebf0d8b9437ec354
Paul Gaddy <35400575+notpaulgaddy@users.noreply.github.com>
2022-12-29 02:33:20 -0500

    Add files via upload

--- src/Components/Merch/Merch.js
+++ src/Components/Merch/Merch.js
@@ -1,16 +1,92 @@
 import React from "react";
+import "./Merch.css";
+import Button from "react-bootstrap/Button";
 import MerchComp from "../MerchComp/MerchComp";
 import NavbarLink from "../NavbarLink/NavbarLink";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { ArrowDown, CheckLg } from "react-bootstrap-icons";
+import model from "./model.png";
+import Jack from "./jack.png"
+import Form from "react-bootstrap/Form";
+import Dua from "./dua.png";
+import Abel from "./abel.png";
+class Merch extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavbarLink />
+        <div class="containerBox">
+          <div className="emailListDiv">
+            <Row>
+              <Col>
+                <Row>
+                  <h2 className="emailListText">
+                    Recieve a 5% Off Coupon
+                    <br />
+                    Upon Joining Our Email
+                    <br />
+                    List
+                  </h2>
+                </Row>
+                <Row>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control type="email" placeholder="Enter email" className="emailBox"/>
+                        <Button><CheckLg/></Button>
+                    </Form.Group>
+                </Row>
+              </Col>
+              <Col>
+                <img src={model} className="modelImg" />
+              </Col>
+            </Row>
+          </div>
+          <Row style={{margin:"auto"}}>
+            <Col>
+              <div className="box1">
+                <img src={Jack} className="jackImg" />
+              </div>
+            </Col>
+            <Col><div className="box2">
+                    <img src={Dua} className="duaImg" />
+                </div></Col>
+            <Col><div className="box3">
+                    <img src={Abel} className="abelImg" />
+                </div></Col>
+          </Row>
+          <div className="topG">
+            <Row>
+              <Col>
+                <DropdownButton id="dropdown-basic-button" title="Sort By">
+                  <Dropdown.Item href="#/action-1">
+                    Price: Lowest - Highest
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Price: Highest - Lowest
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Sold Out</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Available</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Limited Edition
+                  </Dropdown.Item>
+                </DropdownButton>{" "}
+              </Col>
+              <Col>
+                <Button className="filterToggle">
+                  All Filters
+                  <ArrowDown />
+                </Button>
+              </Col>
+            </Row>
+          </div>
 
-class Merch extends React.Component{
-    render(){
-        return(
-            <>
-                <NavbarLink/>
-                <MerchComp/>
-            </>
-        )
-    }
+          <MerchComp />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Merch;
--- src/Components/MerchComp/MerchComp.js
+++ src/Components/MerchComp/MerchComp.js
@@ -4,33 +4,45 @@ import clothingData from"../clothingData.js";
 import { Card } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
-
+import star from "./stars.png";
+import Row from "react-bootstrap/Row";
 const MerchComp = () => {
   const products = clothingData.map(product => {
     let show;
-    if(product.status!=="Available" && product.exclusivity=="Limited Edition"){
-      show = <div><Button variant="warning" style={{margin: "auto"}}>SOLD OUT</Button></div>
-    }else if(product.status=="Available" && product.exclusivity=="Limited Edition"){
-      show = <div><Button variant="warning" style={{margin: "auto"}}>Limited Edition</Button></div>
-    }else if(product.status=="SOLD OUT" && product.exclusivity=="None"){
-      show = <div><Button variant="primary" style={{margin: "auto"}}>SOLD OUT</Button></div>
+    let itemRating;
+    // if(product.status!=="Available" && product.exclusivity=="Limited Edition"){
+    //   show = <div><Button className="aTc" >SOLD OUT</Button></div>
+    // }
+    // else if(product.status=="Available" && product.exclusivity=="Limited Edition"){
+    //   show = <div><Button className="aTc">Limited Edition</Button></div>
+    //}
+    if(product.status=="SOLD OUT"){
+      show = <div><button className="aTc button"><strong>Out of Stock</strong></button></div>
     }else{
-      show = <div><Button style={{margin: "auto",  backgroundColor: "white",border: "none"}}>AVAILABLE</Button></div>
+      show = <div><button className="aTc button"><strong>Add to Cart</strong></button></div>
+    }
+    if(product.rating=="5"){
+      itemRating = <Row className="ratingRow"><img src={star} alt="star" className="star"/><img src={star} alt="star" className="star"/><img src={star} alt="star" className="star"/><img src={star} alt="star" className="star"/><img src={star} alt="star" className="star"/></Row>
+    }else if(product.rating=="4"){
+      itemRating = <Row className="ratingRow"><img src={star} alt="star" className="star"/><img src={star} alt="star" className="star"/><img src={star} alt="star" className="star"/><img src={star} alt="star" className="star"/></Row>
+    }else if(product.rating=="3"){
+      itemRating = <Row className="ratingRow"><img src={star} alt="star" className="star"/><img src={star} alt="star" className="star"/><img src={star} alt="star" className="star"/></Row>
     }
     return(
       <Link to={`/Merch/${product.id}`}>
       <Card style={{ width: "18rem",marginBottom:"10px"}} className="square border border-0">
       <div key={product.id}>
-        <Card.Img variant="top" src={product.image}/>
-        <Card.Img/>
+        <div className="itemBackgroundColor">
+          <Card.Img variant="top" src={product.image}/>
+        </div>
         <Card.Body>
-             <Card.Title style={{textAlign: "center"}}>{product.title}</Card.Title>
-             <Card.Text style={{textAlign: "center",marginBottom:"20px"}}>{product.text}</Card.Text>
+             <Card.Title style={{display:"inline",color:"black"}}>{product.title} <sup>$</sup>{product.text}<sup>.00</sup></Card.Title>
+              <Card.Text>{itemRating}</Card.Text>
              <Card.Text className="limitedButton col text-center">{show}</Card.Text>
            </Card.Body>
       </div>
       </Card>
-      </Link>
+      </Link> 
     )
   })
   return (
